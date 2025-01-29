3739119c07171d9fe67ba36fb556322a43cfe5ff
Anish-Dangol <anis.dangol121@gmail.com>
2025-01-29 22:53:25 +0545

    Server Setup Added

---
+++ server/server.js
@@ -0,0 +1,36 @@
+import express from "express";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+// Create a database connection -> u can also
+// Create a separate file for this and tehn import/use that file here
+
+mongoose.connect('mongodb+srv://anisdangol121:bgnBFpOsRhscKDPY@e-commerce.1h459.mongodb.net/')
+.then(() => console.log('MongoDB connected'))
+.catch(error => console.log(error));
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(
+    cors({
+        origin : 'http://localhost:5173/',
+        methods : ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders : [
+            "content-type",
+            'Authorization',
+            'Cache-Control',
+            'Expires',
+            'Pragma',
+        ],
+        credentials : true,
+    })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`Server is now running on port ${PORT}`));
+
+
