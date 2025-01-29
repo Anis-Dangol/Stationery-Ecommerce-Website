aa00edc2048f98f3ac7dfe92d2bb51304b60b567
Anish-Dangol <anis.dangol121@gmail.com>
2025-01-30 00:45:22 +0545

    Form UI Added

---
+++ client/src/config/index.js
@@ -0,0 +1,41 @@
+
+export const registerFormControls = [
+    {
+        name: 'userName',
+        label: 'User Name',
+        placehoder: 'Enter your user name',
+        componentType : 'input',
+        type : 'text',
+    },
+    {
+        name: 'email',
+        label: 'Email',
+        placehoder: 'Enter your email',
+        componentType : 'input',
+        type : 'email',
+    },
+    {
+        name: 'password',
+        label: 'Password',
+        placehoder: 'Enter your password',
+        componentType : 'input',
+        type : 'password',
+    }
+]
+
+export const loginFormControls = [
+    {
+        name: 'email',
+        label: 'Email',
+        placehoder: 'Enter your email',
+        componentType : 'input',
+        type : 'email',
+    },
+    {
+        name: 'password',
+        label: 'Password',
+        placehoder: 'Enter your password',
+        componentType : 'input',
+        type : 'password',
+    }
+]
--- client/src/pages/auth/register.jsx
+++ client/src/pages/auth/register.jsx
@@ -1,9 +1,45 @@
+import CommonForm from "@/components/common/form";
+import { registerFormControls } from "@/config";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
+const initialState = {
+    userName : '',
+    email : '',
+    password : ''
+}
 
 
 function AuthRegister() {
+
+    const [formData, setFormData] = useState(initialState);
+
+    function onSubmit() {
+
+    }
+
     return ( 
-        <div>Register</div>
+        <div className="mx-auto w-full max-w-md space-y-6">
+            <div className="text-center">
+                <h1 className="text-3xl font-bold tracking-tight text-foreground">Create a new account</h1>
+            </div>
+            <CommonForm
+            formControls={registerFormControls}
+            buttonText={'Sign Up'}
+            formData={formData}
+            setFormData={setFormData}
+            onSubmit={onSubmit}
+            />
+            <p className="mt-0">
+                    Already have an account
+                    <Link 
+                        className="font-medium ml-2 text-primary hover:underline" 
+                        to="/auth/login"
+                    >
+                        Login
+                    </Link>
+                </p>
+        </div>
      );
 }
 
